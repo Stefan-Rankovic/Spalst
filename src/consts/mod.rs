3a157ee111c43f79d86f3f170f86545fccd18e7a
Stefan-Rankovic <stefi.rankovic@proton.me>
2025-11-14 23:22:59 +0100

    cleaned up and fixed the consts/ directory that hasn't been touched for a long time

--- src/consts/mod.rs
+++ src/consts/mod.rs
@@ -1,7 +1,5 @@
-pub mod bad_versions;
 pub mod github_repo_name;
 pub mod github_repo_owner;
 
-pub use bad_versions::BAD_VERSIONS;
 pub use github_repo_name::GITHUB_REPO_NAME;
 pub use github_repo_owner::GITHUB_REPO_OWNER;
