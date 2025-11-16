c151981bd184c64471a34280ef7f42ab9dcea662
Stefan-Rankovic <stefi.rankovic@proton.me>
2025-11-16 16:22:18 +0100

    improved entity templates by making their name be a special struct, and also fixed a bug when reading an entity template

---
+++ src/structs/entity_template_name.rs
@@ -0,0 +1,10 @@
+use serde::Deserialize;
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, Hash, PartialEq)]
+pub struct EntityTemplateName(pub String);
+impl Display for EntityTemplateName {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
