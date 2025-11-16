c9305dd9b9450b9a5c5376cb51f289ab08f6be2d
Stefan-Rankovic <stefi.rankovic@proton.me>
2025-11-16 22:37:40 +0100

    removed the old handle_event code

--- src/structs/app/handle_event/main_menu/load_playthrough.rs
+++
@@ -1,9 +0,0 @@
-use crate::structs::App;
-use color_eyre::eyre::Result;
-use ratatui::crossterm::event::Event;
-
-impl App {
-    pub fn main_menu_load_playthrough_handle_event(&self, event: Event) -> Result<()> {
-        todo!();
-    }
-}
