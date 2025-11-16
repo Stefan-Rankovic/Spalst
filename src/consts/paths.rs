203fa01f26facd48270d4af8eb30a3dad35c8c33
Stefan-Rankovic <stefi.rankovic@proton.me>
2025-11-16 22:14:00 +0100

    modified the logic of game paths so they're more centralized in one consts/paths.rs file

---
+++ src/consts/paths.rs
@@ -0,0 +1,2 @@
+pub const ENTITY_TEMPLATES_PATH: &str = "assets/entity_templates/";
+pub const SPALST_SAVE_PATH: &str = "spalst_save.ron";
--- src/structs/asset.rs
+++ src/structs/asset.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Asset {
-    name: String,
-    browser_download_url: String,
+    pub name: String,
+    pub browser_download_url: String,
     size: u64,
 }
