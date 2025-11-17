a7fb22a01d88942fa524034c8f26105b48e0538d
Stefan-Rankovic <stefi.rankovic@proton.me>
2025-11-17 22:45:23 +0100

    moved consts to one file and added a const (I couldn't bother to move it to the right commit it doesn't matter anyway probably)

---
+++ src/consts.rs
@@ -0,0 +1,12 @@
+/// SPDX-License-Identifier: GPL-3.0-only
+
+// GitHub
+pub const GITHUB_REPO_OWNER: &str = "Stefan-Rankovic";
+pub const GITHUB_REPO_NAME: &str = "Spalst";
+
+// Paths
+pub const ENTITY_TEMPLATES_PATH: &str = "assets/entity_templates/";
+pub const SPALST_SAVE_PATH: &str = "spalst_save.ron";
+
+// Time
+pub const CREATE_PLAYTHROUGH_WARN_TIME: f64 = 3.5;
