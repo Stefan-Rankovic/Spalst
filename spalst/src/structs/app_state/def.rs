b010944d180167b4eafbc045b6e89be1a62eeb35
Stefan Rankovic <stefi.rankovic@proton.me>
2026-08-07 18:52:17 +0000

    feat(structs/app_state/def.rs): The MainMenuScreen is now opened automatically on startup

--- spalst/src/structs/app_state/def.rs
+++ spalst/src/structs/app_state/def.rs
@@ -3,7 +3,7 @@
 
 #[cfg(feature = "logging")]
 use crate::structs::Logger;
-use crate::structs::{ArgsParser, ScreenManager};
+use crate::structs::{ArgsParser, MainMenuScreen, ScreenId, ScreenManager};
 use clap::Parser as _;
 use color_eyre::{Report, eyre::Result};
 
@@ -40,7 +40,8 @@ impl AppState {
         let args: ArgsParser = ArgsParser::parse();
         #[cfg(feature = "logging")]
         let logger: Option<Logger> = args.log.then_some(Logger::try_init_new(&args).await?);
-        let screen_manager: ScreenManager = ScreenManager::new();
+        let mut screen_manager: ScreenManager = ScreenManager::new();
+        let _: ScreenId = screen_manager.add_select_screen(screen_manager.root_id(), Box::new(MainMenuScreen))?;
 
         Ok(Self {
             handle_error: None,
